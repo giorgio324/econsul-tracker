f6e00b55ba8e10d1c865e577a7c0e497a123e165
giorgio324 <giorgikochuashvili2002@gmail.com>
2025-04-18 01:20:08 +0400

    feat: add font with colors in tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <h1 className='text-3xl font-bold underline font-nunito'>Hello world!</h1>
+  );
 };
 
 export default App;
